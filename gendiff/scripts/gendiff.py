c070b15cade6394a97afaf76ee9a8d4c522d45a6
Tragoedia <tragoedia@mail.ru>
2021-09-15 22:03:02 +0700

    step_four

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python3
+"""This is start gendiff."""
 import argparse
+
 from gendiff.gendiff_engine import generate_diff
 
 
 def main():
+    """Print difference of two file."""
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format',
-                        help='set format of output')
+    parser.add_argument('-f', '--format', help='set format of output')
 
     args = parser.parse_args()
     print(generate_diff(args.first_file, args.second_file))
